5b0cc803c53856caa57c07b543279ee7e44c9f9b
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-01-02 07:11:06 +0000

    Added event listener

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,12 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  // Force form redirect
+  const searchForm = document.querySelector('.homepage-hero__search-group').closest('form');
+  if (searchForm) {
+    searchForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      window.location.href = '/ccs-home/content';
+    });
+  }
 })
