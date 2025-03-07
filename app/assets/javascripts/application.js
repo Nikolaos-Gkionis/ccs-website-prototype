13b4f26d66942d989478613e9132d65cf3e19679
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-03-07 11:00:08 +0000

    latest changes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,3 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-  // Force form redirect
-  const searchForm = document.querySelector('.homepage-hero__search-group').closest('form');
-  if (searchForm) {
-    searchForm.addEventListener('submit', function(e) {
-      e.preventDefault();
-      window.location.href = '/ccs-home/content';
-    });
-  }
-})
