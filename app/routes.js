907d3efbfc7da0d83912398011a81212277c32c5
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-03-27 09:42:57 +0000

    latest changes
    - events pages
    -ai pages

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,12 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const events = require('./data/events.json');
 
 // Add your routes here
+
+router.get('/your-route', function (req, res) {
+  res.render('new/events-search', {
+    events: events
+  });
+});
